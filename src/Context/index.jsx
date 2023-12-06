a773094c75de0484d1b01109919bbebc25a87453
Brian Nicolas Pizaro <nico05pizarro@hotmail.com>
2023-12-06 06:57:03 -0300

    funcionalidades product detail

--- src/Context/index.jsx
+++ src/Context/index.jsx
@@ -4,11 +4,18 @@ export const ShopiCartContext = createContext()
 
 export const ShopiCartProvider = ({children}) => {
   const [counter, setCounter] = useState(0)
+  const [isProductDetailOpen, setIsProductDetailOpen ] = useState(false)
+  const openProductDetail = () =>setIsProductDetailOpen(true)
+  const closeProductDetail = () =>setIsProductDetailOpen(false)
+  console.log(isProductDetailOpen)
 
   return (
     <ShopiCartContext.Provider value={{
       counter,
-      setCounter
+      setCounter,
+       openProductDetail,
+       closeProductDetail,
+       isProductDetailOpen
     }}>
       {children}
     </ShopiCartContext.Provider>
--- src/components/ProductDetail/index.jsx
+++ src/components/ProductDetail/index.jsx
@@ -1,11 +1,15 @@
+import { useContext } from 'react'
+import { ShopiCartContext } from '../../Context'
 import './styles.css'
+
 const ProductDetail = ()=> {
+    const context = useContext(ShopiCartContext)
     return(
-        <aside className="product-detail flex flex-col fixed right-0 border border-black rounded-lg bg-white"> 
+        <aside className={`${context.isProductDetailOpen ? 'flex': 'hidden'} product-detail flex-col fixed right-0 border border-black rounded-lg bg-white`}> 
             <div className='flex justify-between items-center p-6'>
                 <h2 className='font-medium text-xl'>Detail</h2>
-                <div><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-  <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
+                <div onClick={()=>context.closeProductDetail()}><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12"/>
 </svg>
 </div>
             </div>
